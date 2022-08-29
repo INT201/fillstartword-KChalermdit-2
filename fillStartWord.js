bc816efe9984c9492f6b9a70a63e0f43eb3e4ef7
Khajonsak Chalermdit <35735538+KChalermdit@users.noreply.github.com>
2022-08-29 20:26:26 +0700

    Update fillStartWord.js

--- fillStartWord.js
+++ fillStartWord.js
@@ -4,6 +4,7 @@ function fillStartWord(startWord, word) {
   if (word === undefined || word === null) return undefined
   
   
-  return word?.startsWith(startWord) ? word : `${startWord}${word}`
+  //return word?.startsWith(startWord) ? word : `${startWord}${word}`
+  return 0
 }
 module.exports = fillStartWord
